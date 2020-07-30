e9c831d64922452093c8a19bdc35afbd7bf28899
Romulo <romulo@iMac-de-iMac.local>
2020-07-30 14:19:37 -0300

    Subindo TikTok

---
+++ src/Screens/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+export default function HomeScreen (){
+  return (
+      <View style={{flex: 1, alignItems:'center', justifyContent:'center'}} >
+          <Text> teste  </Text>
+
+      </View>
+
+  );
+
+
+
+}
